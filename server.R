25c44249841e18ac6ca8b929f19aed07d99f9155
andrewpbray <andrewbray@berkeley.edu>
2023-05-01 11:16:21 -0700

    attempt simplifying shinymeta script

--- server.R
+++ server.R
@@ -4,74 +4,62 @@ library(DT)
 library(shinymeta)
 
 shinyServer(function(input, output, session) {
-  # reads uploaded file
-  data <- metaReactive2({
+  # reads uploaded file and calculates scores for each assignment
+  assignment_scores <- metaReactive2({
     shiny::req(input$file)
+    
     metaExpr({
-      read.csv(..(input$file$datapath))
+      read.csv(..(input$file$datapath)) %>%
+        mutate(Score = round(Raw_Points/Max_Points*100, 2))
     })
   })
-  #original data
-  output$original <- metaRender(renderDataTable, {
-    ..(data())
+
+  # output data table with assignment scores
+  output$assignment_scores_tab <- metaRender(renderDataTable, {
+    ..(assignment_scores())
   })
-  #calculates scores for each assignment
-  means <- metaReactive({
-    ..(data()) |>
-      rowwise() |>
-      mutate(Score = round(Raw_Points/Max_Points*100,2))
-    # mutate(mean = mean(c_across(3:ncol(..(data())))))
+  
+  # agg_fn <- metaReactive2({
+  #   switch(input$grading_policy,
+  #          "Equally Weighted" = rlang::expr(mean(Score)),
+  #          "Weighted by Points" = rlang::expr(sum(Raw_Points)/sum(Max_Points)))
+  # })
+  
+  # calculate student grades
+  student_grades <- metaReactive({
+      ..(assignment_scores()) %>%
+        group_by(name) %>%
+        summarize(Grade = ..(switch(input$grading_policy,
+                                    "Equally Weighted" = rlang::expr(mean(Score)),
+                                    "Weighted by Points" = rlang::expr(sum(Raw_Points)/sum(Max_Points)))))
   })
-  grading <- metaReactive({
-    (..(input$grading_policy) == "Equally Weighted")
+
+  # output table with student grades
+  output$student_grades_tab <- metaRender(renderDataTable, {
+    ..(student_grades())
   })
-  # Output data table with assignment scores
-  output$table <- metaRender(renderDataTable, {
-    ..(means())
-  })
-  #calculates grades based on criteria
-  grades_table <- metaReactive({
-    test <- ..(means())
-    test <- (unique(test[1]))
-    colnames(test) <- "Names"
-    table <- data.frame(Names = test,
-                        Grades = 0) #table with each unique student
-    if (..(grading())){
-      table <- ..(means()) |>
-        group_by(name) |>
-        summarize(Grade = mean(Score)) #grades with equal weights of each assignment
-    } else {
-      table <- ..(means()) |>
-        group_by(name) |>
-        summarize(Grade = sum(Raw_Points)/sum(Max_Points)) %>%
-        mutate(Grade = round(Grade*100, 2)) #grades with weights based on points
-    }
-    return (table)
-  })
-  #outputs table with student grades
-  output$grades <- metaRender(renderDataTable, {
-    ..(grades_table())
-  })
-  # download grades as CSV
-  output$download <- downloadHandler(
-    filename = function() {
-      paste("grades_", Sys.Date(), ".csv", sep = "")
-    },
-    content = function(file) {
-      write.csv(grades_table(), file, row.names = FALSE)
-    }
-  )
+  
+  # # download grades as CSV
+  # output$download <- downloadHandler(
+  #   filename = function() {
+  #     paste("grades_", Sys.Date(), ".csv", sep = "")
+  #   },
+  #   content = function(file) {
+  #     write.csv(grades_table(), file, row.names = FALSE)
+  #   }
+  # )
+  
   #outputs code
   output$code <- renderPrint({
-    expandChain(quote(library(tidyverse)), output$table(), output$grades())
+    expandChain(quote(library(dplyr)), output$student_grades_tab())
   })
+  
   #downloading code
   output$downloadr <- downloadHandler(
     filename = "report.zip",
     content = function(file) {
       report <- output$code()
       buildScriptBundle(report, file)
-    }
-  )
+    })
 }
 )
