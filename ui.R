c85115882207cd8deeb2ffe2f02e53cb596ee2ae
andrewpbray <andrewbray@berkeley.edu>
2023-05-01 11:15:58 -0700

    simplify ui and remove tabs

--- ui.R
+++ ui.R
@@ -5,7 +5,7 @@ shinyUI(
   fluidPage(
   # App title
   titlePanel("Calculate Grade"),
-  # Sidebar with file input
+  
   sidebarLayout(
     sidebarPanel(
       fileInput("file", "Upload CSV file"), #upload file
@@ -15,22 +15,18 @@ shinyUI(
     ),
     # Main panel with table output
     mainPanel(
-      tabsetPanel(
-        #shows original csv
-        tabPanel(h5("Original Data"),
-                 DT::dataTableOutput("original")),
-        #shows table with assignment scores calculated
-        tabPanel(h5("Calculated Scores"),
-                 DT::dataTableOutput("table")),
-        #shows table with students' overall grade + download button for this table
-        tabPanel(h5("Grades"),
-                 DT::dataTableOutput("grades"),
-                 downloadButton("download", "Download Results"),),
-        #shows meta code + download button for this code
-        tabPanel(h5("Code"),
-                 verbatimTextOutput("code"),
-                 downloadButton("downloadr", "download R code"))
-      )
+      
+      #shows table with assignment scores calculated
+      h4("Assignment Scores"),
+      DT::dataTableOutput("assignment_scores_tab", width = "70%"),
+      
+      #shows table with students' overall grade + download button for this table
+      h4("Student Grades"),
+      DT::dataTableOutput("student_grades_tab", width = "40%"),
+      
+      #shows meta code + download button
+      verbatimTextOutput("code"),
+      downloadButton("downloadr", "download script")
     )
   )
 )
